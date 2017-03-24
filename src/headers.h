8e5343f6df584489232d98c4cbfa62565c0397a5
Felix-t <felix.tamagny@ifsttar.fr>
2017-03-24 14:44:29 +0100

    LSM9DS0 and battery mgmt OK, compilation OK, to testing && module integration

--- src/headers.h
+++ src/headers.h
@@ -1,27 +1,31 @@
+#ifndef HEADER_H
+#define HEADER_H
+
 /*
  * startup.c:
  *	Program launched on startup 
  *	 - Check remaining battery power
  *	 - Schedule next startup and shutdown 
  *	 - Start acquisitions subroutines
- *  @TODO : Logging system, see syslog
  */
 #include <sys/types.h>
 #include <sys/syscall.h>
 #include <sys/wait.h>
 
-#include <pthread.h>
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <errno.h>
 #include <stdlib.h>
-#include <syslog.h>
-#include <unistd.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <bcm2835.h>
+#include <libconfig.h>
+#include <pthread.h>
 
 #define MAG_ACQ 0
 #define ACC_GYR 1
 #define WLX2 0
 
-int8_t end_program;//To be modified only by main program to shutdown other threads
+extern int8_t end_program;//To be modified only by main program to shutdown other threads
+
+#endif
