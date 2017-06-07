2eac0ac28067ea8c6728217e15c13b1ef300dfac
Felix-t <felix.tamagny@ifsttar.fr>
2017-06-07 17:12:35 +0200

    Downlink working

--- tests/test.c
+++ tests/test.c
@@ -7,62 +7,143 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <byteswap.h>
 
 static uint8_t set_interface_attribs (int fd, int speed, int parity);
 static uint8_t set_blocking (int fd, int should_block);
+static void parse_downlink(uint8_t *msg_received);
+static void receive_downlink(int fd, uint8_t *msg_received);
+
+static void receive_downlink(int fd, uint8_t *msg_received)
+{
+	FILE *fp = fopen("log_tests", "a");
+	FILE *fp2 = fopen("received", "a");
+	uint8_t i,bytes_rcv,  save = 0, bytes_ack = 0;
+	uint8_t rcv_msg[100];
+	write(fd, "+++", 3);
+	sleep(2);
+	write(fd, "ATS000=d0\n", 10);
+	sleep(2);
+	write(fd, "ATQ\n", 4);
+	sleep(2);
+	write(fd, "ACK", 3);
+	sleep(60);
+	fprintf(fp, "Checking for Aknowledgment... \n");
+	printf("Mark 1\n");
+	do
+	{
+		bytes_rcv = read(fd, rcv_msg, 100);
+
+		for(i=0 ; i<bytes_rcv ; i++)
+		{
+			if(save && bytes_ack < 8)
+			{
+				msg_received[bytes_ack++] = rcv_msg[i];
+				fprintf(fp2, "%i\t", rcv_msg[i]);
+			}
+
+			else if(i > 1 
+					&& rcv_msg[i-8] == 'T' 
+					&& rcv_msg[i-7] == ' ' 
+					&& rcv_msg[i-6] == 'S' 
+					&& rcv_msg[i-5] == 'E' 
+					&& rcv_msg[i-4] == 'T' 
+					&& rcv_msg[i-3] == 'U' 
+					&& rcv_msg[i-2] == 'P' )
+					//&& rcv_msg[i-1] == '\r' 
+					//&& rcv_msg[i] == '\n')
+				save = 1;
+			fprintf(fp, "%c", rcv_msg[i]);
+
+		}
+	}while(bytes_rcv != 0);
+	printf("Mark 2\n");
+	sleep(2);
+	write(fd, "+++", 3);
+	sleep(2);
+	write(fd, "ATS000=50\n", 10);
+	sleep(2);
+	write(fd, "ATQ\n", 4);
+	sleep(2);
+	fclose(fp);
+	fprintf(fp2, "\n");
+	printf("Mark 3\n");
+	fclose(fp2);
+}
+
+
+static void parse_downlink(uint8_t *msg_received)
+{
+	FILE *fp = fopen("log_tests", "a");
+	char cfg_str[100] = "";
+	uint32_t tmp;
+	float cfg_val;
+	switch(msg_received[0]){
+	case 0:
+		strcat(cfg_str, "ACQ_TIME");
+		break;
+	case 1:
+		strcat(cfg_str, "MIN_VOLT");
+		break;
+	case 2:
+		strcat(cfg_str, "MIN_VOLT");
+		break;
+	case 3:
+		strcat(cfg_str, "THRESHOLD");
+		break;
+	case 4:
+		strcat(cfg_str, "freq_echantillonnage");
+		break;
+	case 5:
+		strcat(cfg_str, "zeros");
+		break;
+	default:
+		strcat(cfg_str, "ACQ_TIME");
+		break;
+	}
+	printf("%p\n", &cfg_val);
+	tmp = msg_received[1] << 24 
+		| (msg_received[2] << 16) 
+		| (msg_received[3] << 8) 
+		| msg_received[4]; 
+	memcpy(&cfg_val, &tmp, 4);
+	fprintf(fp, "Set config %s to %f\n", cfg_str, cfg_val);
+	//set_cfg((char **) &cfg_str, (float) &cfg_val, 1);
+	sleep(2);
+	fclose(fp);
+
+}
+
 
 int main()
 {
-	sleep(10);
 	int i = 0, u = 1, j = 0;
 
-	FILE *fp = fopen("log_tests", "a");
+	uint8_t msg[8] = {0x02,0x40,0x80,2,3,0,0,0};
+	uint32_t tmp = 0;
+	tmp = msg[1] << 24 | (msg[2] << 16) | (msg[3] << 8) | msg[4]; 
 
-	char *portname = "/dev/ttyUSB0";
+	float a ;
+	memcpy(&a, &tmp, 4);
+	printf("%f\n", a);
+	char *portname = "/dev/SGF";
 	uint8_t in_messages[500];
-	uint8_t out_messages[9][12] = {
-		{0,1,2,3,4,5,6,7,8,9,10,11},
-		{'+','+','+'},
-		{'A','T','S','0','0','0','=','d','0','\n'},
-		{'A','T','Q','\n'},
-		{'a','b','c','d','e','f','g','h','i','j','k','l'},
-		{'+','+','+'},
-		{'A','T','S','0','0','0','=','5','0','\n'},
-		{'A','T','Q','\n'},
-		{0,1,2,3,4,5,6,7,8,9,10,11}};
+	uint8_t msg_rcv[8];
+
 
 	int fd = open (portname, O_RDWR | O_NOCTTY | O_SYNC);
-	set_interface_attribs (fd, B9600, 0);  // set speed to 115,200 bps, 8n1 (no parity)
+	set_interface_attribs (fd, B19200, 0);  // set speed to 115,200 bps, 8n1 (no parity)
 	set_blocking (fd, 0);                // set no blocking
-	
-	char * str = "$PMTK300,1000,0,0,0,0*1C\r\n";
-	write(fd, str, strlen(str));
-	str = "$PMTK220,1000*1F\r\n";
-	write(fd, str, strlen(str));
-	str = "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28";
-	write(fd, str, strlen(str));
-	str = "$PMTK313,1*2E\r\n";
-	write(fd, str, strlen(str));
-	str = "$PMTK301,2*2E\r\n";
-	write(fd, str, strlen(str));
+	receive_downlink(fd, msg_rcv);
+	parse_downlink(msg_rcv);
+	char send[8];
+	sprintf(send, "%f", (float) msg_rcv[1]);
+
+	write(fd, send, 3);
 	sleep(8);
-	//set_interface_attribs (fd, B38400, 0);  // set speed to 115,200 bps, 8n1 (no parity)
-   struct timespec tt = {
-               .tv_sec = 0,        /* seconds */
-               .tv_nsec = 500000000       /* nanoseconds */
-           };
 
-	while(j < 200)
-	{
-		u = read(fd, in_messages, 500);
 
-		for(i=0;i<u;i++)
-			fprintf(fp, "%c", in_messages[i]);
-		nanosleep(&tt, NULL);
-		j++;
-	}
 	printf("All out_messages sent\n");
-	fclose(fp);
 	close (fd);
 }
 
